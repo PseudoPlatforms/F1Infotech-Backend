964c113928a371714e01ef703c220d6054264d58
sshreya123 <shreyacic123@gmail.com>
2026-08-07 14:02:57 +0530

    Changes done in backend

--- app/schemas/career.py
+++ app/schemas/career.py
@@ -20,6 +20,17 @@ class CareerResponse(BaseModel):
     resume_path: Optional[str] = None
     resume_filename: Optional[str] = None
     created_at: datetime
-
+class CareerUpdate(BaseModel):
+    name: str
+    email: EmailStr
+    phone: Optional[str] = None
+    position: Optional[str] = None
+    experience: Optional[str] = None
+    current_company: Optional[str] = None
+    current_designation: Optional[str] = None
+    notice_period: Optional[str] = None
+    current_ctc: Optional[str] = None
+    expected_ctc: Optional[str] = None
+    message: Optional[str] = None
     class Config:
         from_attributes = True
--- app/schemas/query.py
+++ app/schemas/query.py
@@ -1,24 +1,6 @@
 from pydantic import BaseModel, EmailStr
 from typing import Optional
 from datetime import datetime
-from typing import Optional
-
-
-
-class QueryCreate(BaseModel):
-    first_name: str
-    last_name: str
-    email: EmailStr
-    phone: Optional[str] = None
-    company: str
-    message: str
-
-
-class QueryResponse(QueryCreate):
-    id: int
-
-    class Config:
-        from_attributes = True
 
 
 class QueryCreate(BaseModel):
@@ -32,6 +14,7 @@ class QueryCreate(BaseModel):
 
 class QueryResponse(QueryCreate):
     id: int
+    status: str
     created_at: datetime
 
     class Config:
